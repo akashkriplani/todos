16235a8b4d1a2af01c2a1d0aeb218123ede58bc4
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-22 13:02:54 +0530

    Create a boilerplate for todos app

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import CreateTask from './CreateTask';
+import TaskList from './TaskList';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tasks: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todos</h1>
+        <div>
+          <CreateTask />
+          <br />
+          <TaskList />
+        </div>
+      </div>
+    );
+  }
+}
