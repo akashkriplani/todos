fb87b4726a5557dda31b26eea84e530e043a1020
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-22 13:14:59 +0530

    Add a create task form and handle form submit on CreateTask component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import CreateTask from './CreateTask';
 import TaskList from './TaskList';
 
+const tasks = [];
+
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = { tasks: [] };
   }
 
+  createTask = (task) => {
+    if (task.trim() === '') {
+      alert('Task cannot be empty');
+      return;
+    }
+
+    tasks.push({ task, isCompleted: false });
+    this.setState({ tasks });
+  };
+
   render() {
     return (
       <div>
         <h1>Todos</h1>
         <div>
-          <CreateTask />
+          <CreateTask createTask={this.createTask} />
           <br />
           <TaskList />
         </div>
